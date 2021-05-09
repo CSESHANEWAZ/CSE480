b2e22399a2d425da1eabe0f42943fd4f767cacd6
Muhammad Shanewaz <2016-3-60-050@std.ewubd.edu>
2021-05-10 01:16:04 +0600

    commit

--- phpProjects/php-oop/encapsulation/constructor-3.php
+++ phpProjects/php-oop/encapsulation/constructor-3.php
@@ -5,6 +5,27 @@
         private $username;
         private $location;
         private $homepage;
+
+        //constructor method
+        public function __construct($un, $lc, $hp)
+        {
+            $this -> username = $un;
+            $this -> location = $lc;
+            $this -> homepage = $hp;
+        }
+
+        public function get_profile()
+        {
+            echo "<b>Username:</b> " . $this -> username . "<br>";
+            echo "<b>Location:</b> " . $this -> location . "<br>";
+            echo "<b>Url: </b> <a href= $this->homepage> $this->homepage </a> </br>"; 
+        }
+
     }
+
+    //create an object
+    $member = new Member("cseshanewaz", "Chittagong", "https://cseshanewaz.me");
+
+    $member -> get_profile();
 
 ?>
